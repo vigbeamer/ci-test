64898f4c489a97a68e92049f160f2c584c1119dd
vigbeamer <vignesh.shanmugavelu@getbeamer.com>
2024-08-15 20:32:25 +0530

    reduce flackiness in interacting with iframes

--- e2e/tests/apps/Auth.spec.ts
+++ e2e/tests/apps/Auth.spec.ts
@@ -22,10 +22,13 @@ test("Create User", async ({ page, authPages }) => {
     .getByText("Improve in-app customer onboarding")
     .click();
 
-  await page
-    .frameLocator('iframe[class="userflowjs-bubble__frame"]')
-    .getByTestId("close-button")
-    .click();
+  const tooltipFrame = await page.frameLocator(
+    'iframe[class="userflowjs-bubble__frame"]'
+  );
+  const closeButton = tooltipFrame.getByTestId("close-button");
+
+  await closeButton.waitFor({ state: "visible" });
+  await closeButton.click();
 
   await page.getByRole("button", { name: name }).click({ force: true });
   await page.getByRole("link", { name: "Sign out" }).click({ force: true });
